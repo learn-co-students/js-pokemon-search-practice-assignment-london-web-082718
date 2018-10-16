1b2372d62361bfa9bb919a825d7cd5338cc20447
github username <github email address>
2018-10-16 16:04:19 +0100

    Done.

--- src/pokemon.js
+++ src/pokemon.js
@@ -2,6 +2,45 @@ const POKEMON = [
   {
     height: 10,
     weight: 130,
+    id: 1,
+    name: "bulbusaur",
+    abilities: ["overgrow", "chlorophyll"],
+    moves: [],
+    stats: [
+      {
+        value: 80,
+        name: "special-defense"
+      },
+      {
+        value: 80,
+        name: "special-attack"
+      },
+      {
+        value: 63,
+        name: "defense"
+      },
+      {
+        value: 62,
+        name: "attack"
+      },
+      {
+        value: 60,
+        name: "speed"
+      },
+      {
+        value: 60,
+        name: "hp"
+      }
+    ],
+    types: ["grass", "poison"],
+    sprites: {
+      front: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+      back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png"
+    }
+  },
+  {
+    height: 10,
+    weight: 130,
     id: 2,
     name: "ivysaur",
     abilities: ["overgrow", "chlorophyll"],
